c774f135085dc24f17d686e745f6699cfb3f700d
david <davidindata@gmail.com>
2022-09-30 17:58:34 +0000

    commit

--- test2.py
+++ test2.py
@@ -5,6 +5,6 @@ PORT = 8002
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("<my_ip", PORT), Handler) as httpd:
+with socketserver.TCPServer(("David", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
